f38d842a7edff86e507c80213d120834ba26f915
Ezequiel <barreraezequiel281@gmail.com>
2024-10-20 20:20:14 -0300

    correciones

--- managers/carts-manager.js
+++ managers/carts-manager.js
@@ -1,13 +1,15 @@
 const fs = require('fs')
+const prodManager = require('./products-manager')
 
-class CartManager{
-    constructor(path){
+const pManager = new prodManager('./public/products.json')
+class CartManager {
+    constructor(path) {
         this.path = path
         this.carts = []
         this.products = []
     }
-    
-    #read(){
+
+    #read() {
         try {
             this.carts = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
         } catch (err) {
@@ -15,7 +17,7 @@ class CartManager{
         }
     }
 
-    #write(){
+    #write() {
         try {
             fs.writeFileSync(this.path, JSON.stringify(this.carts, null, 2, 'utf-8'))
         } catch (err) {
@@ -23,49 +25,57 @@ class CartManager{
         }
     }
 
-    addCart(){
+    addCart() {
         this.#read()
-        const cartId = this.carts.length == 0 ? 1 : this.carts[this.carts.length - 1].id +1
-        const cart = {id: cartId, products: this.products}
+        const cartId = this.carts.length == 0 ? 1 : this.carts[this.carts.length - 1].id + 1
+        const cart = { id: cartId, products: this.products }
         this.carts.push(cart)
         this.#write()
         console.log('cart created successfully');
         return cart
     }
 
-    getCartById(id){
+    getCartById(id) {
         this.#read()
         const cartFound = this.carts.find(cart => cart.id === id)
-        if(cartFound){
+        if (cartFound) {
             return cartFound
-        }else{
+        } else {
             console.log('cart not found');
         }
     }
 
-    addProductToCart(cartId, productId){
+    addProductToCart(cartId, productId) {
         this.#read()
         const cartFound = this.getCartById(cartId)
-        if(!cartFound){
+        if (!cartFound) {
             console.log('cart not found');
             return false;
         }
-        
-        const prodInCart = cartFound.products.find(item => item.product.id === productId)
-        
-            if(prodInCart){
+
+        const prodExists = pManager.getProductsByID(productId)
+
+        if (prodExists) {
+            const prodInCart = cartFound.products.find(item => item.product.id === productId)
+
+
+            if (prodInCart) {
                 prodInCart.product.quantity++
                 this.#write()
                 console.log('product added successfully');
                 return prodInCart
-            }else{
-                const newProduct = {id: productId, quantity: 1}
-                cartFound.products.push({product: newProduct})
+            } else {
+                const newProduct = { id: productId, quantity: 1 }
+                cartFound.products.push({ product: newProduct })
                 this.#write()
                 console.log('product added successfully');
                 return newProduct
             }
-        
+        } else {
+            console.log('product does not exist')
+            return false
+        }
+
     }
 }
 
